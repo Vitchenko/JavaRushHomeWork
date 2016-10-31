eedfb7642fe79e41833ac33650e1a8cc07bdbb7b
Vitchenko <Euraeuspa12!>
2016-10-31 13:21:41 +0200

    commit4

--- src/com/javarush/test/level22/lesson09/task02/Solution.java
+++ src/com/javarush/test/level22/lesson09/task02/Solution.java
@@ -1,5 +1,7 @@
 package com.javarush.test.level22.lesson09.task02;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 /* Формируем Where
@@ -12,7 +14,36 @@ import java.util.Map;
 */
 public class Solution {
 
+    public static void main(String[] args)
+    {
+        Map<String, String> map=new LinkedHashMap<>();
+        map.put("name", null);
+        map.put("country", null);
+        map.put("city", null);
+        map.put("age", null);
+        map.put("height", null);
+        System.out.println(getCondition(map));
+    }
+
+
     public static StringBuilder getCondition(Map<String, String> params) {
-        return null;
+
+        StringBuilder sb=new StringBuilder();
+        StringBuilder sb2=new StringBuilder();
+        for (Map.Entry<String, String> pair : params.entrySet())
+        {
+
+            String key = pair.getKey();                      //ключ
+            String value = pair.getValue();                  //значение
+            if(value!=null && !value.isEmpty()){
+                sb.append(key+" = "+"'"+value+"'"+" and ");
+            }
+
+        }
+        if (sb.length()>0){
+        sb.delete(sb.lastIndexOf("and")-1,sb.length());}
+        //sb2.append("\""+sb+"\"");
+
+        return sb;
     }
 }
