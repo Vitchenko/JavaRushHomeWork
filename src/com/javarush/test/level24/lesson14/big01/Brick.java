e8327b9945f2c36202ab3130ba10e7318dab9062
Vitchenko <Euraeuspa12!>
2016-11-07 14:25:17 +0200

    commit10

--- src/com/javarush/test/level24/lesson14/big01/Brick.java
+++ src/com/javarush/test/level24/lesson14/big01/Brick.java
@@ -1,27 +1,39 @@
 package com.javarush.test.level24.lesson14.big01;
 
 /**
- * Created by viv on 26.10.2016.
+ * Класс для объекта "кирпич".
  */
 public class Brick extends BaseObject
 {
-    int x;
-    int y;
-    int size;
+    //картинка для отрисовки
+    private static int[][] matrix = {
+            {0, 0, 0, 0, 0},
+            {0, 1, 1, 1, 0},
+            {0, 1, 1, 1, 0},
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0},
+    };
 
-    public Brick(double x, double y, double radius)
+    public Brick(double x, double y)
     {
-        super(x, y, radius);
+        super(x,y,3);
     }
 
+    /**
+     * Рисуем себя на холсте
+     */
     @Override
     public void draw(Canvas canvas)
     {
-
+        canvas.drawMatrix(x - radius + 1, y, matrix, 'H');
     }
 
-    public void move(){
-    }
-    public void draw(){
+    /**
+     * Ничего не делаем - кирпич неподвижен
+     */
+    @Override
+    public void move()
+    {
+        //do nothing
     }
 }
--- src/com/javarush/test/level24/lesson14/big01/Canvas.java
+++ src/com/javarush/test/level24/lesson14/big01/Canvas.java
@@ -1,46 +1,90 @@
 package com.javarush.test.level24.lesson14.big01;
 
 /**
- Задание 10
- Что мы будем делать с Canvas?
- Мы будем рисовать на нем (в его матрице).
- Поэтому нам понадобятся два метода
- public void setPoint(double x, double y, char c)
- public void drawMatrix(double x, double y, int[][] matrix, char c)
-
- Первый метод - setPoint будет "ставить точку в координатах x,y цветом c".
- В методе надо:
- а) округлить x и y до целых чисел
- б) занести в matrix[y][x] значение с
- в) ничего не делать, если x<0 или y<0 или y>matrix.length или x>matrix[0].length
-
- Второй метод - drawMatrix копирует переданную ему картинку (матрицу) в матрицу Canvas.
- И не просто копирует, а начиная с координат x, y
- В методе надо:
- а) с помощью двух вложенных циклов пройтись по всем ячейкам переданной картинки
- б) если значение ячейки matrix[i][j] не равно 0, то покрасить в матрице объекта Canvas точку (x+j, y+i) в цвет c:
- setPoint(x+j, y+i, c)
+ * Класс-холст для отрисовки.
  */
 public class Canvas
 {
-    int width,height;
+    //ширина и высота
+    private int width;
+    private int height;
+    //матрица, где рисуем. символ - это цвет.
+    private char[][] matrix;
 
     public Canvas(int width, int height)
     {
         this.width = width;
         this.height = height;
+        this.matrix = new char[height + 2][width + 2];
     }
 
-    char[][] matrix;
+    /**
+     * Очищаем холст
+     */
+    public void clear()
+    {
+        this.matrix = new char[height + 2][width + 2];
+    }
+
+    /**
+     * Печатаем переданную фигуру в указанных координатах уветом c.
+     * Если переданный массив содержит единицы, то на холсте им будут соответствовать символы - с.
+     */
+    public void drawMatrix(double x, double y, int[][] matrix, char c)
+    {
+        int height = matrix.length;
+        int width = matrix[0].length;
+
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                if (matrix[i][j] == 1)
+                    setPoint(x + j, y + i, c);
+            }
+        }
+    }
+
+    /**
+     * Ставим одну точку на холсте с координатами (x,y) и цветом - c.
+     */
+    public void setPoint(double x, double y, char c)
+    {
+        int x0 = (int) Math.round(x);
+        int y0 = (int) Math.round(y);
+        if (y0 < 0 || y0 >= matrix.length) return;
+        if (x0 < 0 || x0 >= matrix[y0].length) return;
+
+        matrix[y0][x0] = c;
+    }
+
+    /**
+     * Печатаем содержимое холста на экран.
+     */
+    public void print()
+    {
+        System.out.println();
+
+        for (int i = 0; i < height + 2; i++)
+        {
+            for (int j = 0; j < width + 2; j++)
+            {
+                System.out.print(" ");
+                System.out.print(matrix[i][j]);
+                System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
 
     public int getWidth()
     {
         return width;
-    }
-
-    public void setWidth(int width)
-    {
-        this.width = width;
     }
 
     public int getHeight()
@@ -48,34 +92,8 @@ public class Canvas
         return height;
     }
 
-    public void setHeight(int height)
-    {
-        this.height = height;
-    }
-
     public char[][] getMatrix()
     {
         return matrix;
-    }
-
-    public void setMatrix(char[][] matrix)
-    {
-        this.matrix = matrix;
-    }
-
-    /**
-     *  Первый метод - setPoint будет "ставить точку в координатах x,y цветом c".
-     В методе надо:
-     а) округлить x и y до целых чисел
-     б) занести в matrix[y][x] значение с
-     в) ничего не делать, если x<0 или y<0 или y>matrix.length или x>matrix[0].length*/
-
-    public void setPoint(double x, double y, char c){
-        int xx=(int)x;
-        int yy=(int)y;
-
-    }
-    public void drawMatrix(double x, double y, int[][] matrix, char c){
-
     }
 }
