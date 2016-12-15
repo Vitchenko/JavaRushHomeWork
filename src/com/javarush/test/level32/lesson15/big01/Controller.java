8412fb1436f9af8d98791aa5805b2e12847728a8
Vitchenko <Euraeuspa12!>
2016-12-15 20:01:03 +0200

    task_10

--- src/com/javarush/test/level32/lesson15/big01/Controller.java
+++ src/com/javarush/test/level32/lesson15/big01/Controller.java
@@ -1,8 +1,10 @@
 package com.javarush.test.level32.lesson15.big01;
 
+import javax.swing.*;
 import javax.swing.text.html.HTMLDocument;
 import javax.swing.text.html.HTMLEditorKit;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.StringReader;
 import java.io.StringWriter;
 
@@ -133,7 +135,32 @@ public class Controller
     {
     }
 
-    public void saveDocumentAs()
-    {
+    public void saveDocumentAs() {
+
+        //Переключать представление на html вкладку
+        view.selectHtmlTab();
+        //Создавать новый объект для выбора файла JFileChooser
+        JFileChooser jFileChooser = new JFileChooser();
+        //Устанавливать ему в качестве фильтра объект HTMLFileFilter
+        jFileChooser.setFileFilter(new HTMLFileFilter());
+        //Показывать диалоговое окно "Save File" для выбора файла
+        int n = jFileChooser.showSaveDialog(view);
+
+        //Если пользователь подтвердит выбор файла:
+        if (n == JFileChooser.APPROVE_OPTION) {
+            //Сохранять выбранный файл в поле currentFile
+            currentFile = jFileChooser.getSelectedFile();
+            //Устанавливать имя файла в качестве заголовка окна представления
+            view.setTitle(currentFile.getName());
+
+            //Создавать FileWriter на базе currentFile
+            try (FileWriter fileWriter = new FileWriter(currentFile)) {
+                //Переписывать данные из документа document в объекта FileWriter-а аналогично тому, как мы это делали в методе getPlainText()
+                new HTMLEditorKit().write(fileWriter, document, 0, document.getLength());
+            }
+            catch (Exception e) {
+                ExceptionHandler.log(e);
+            }
+        }
     }
 }
