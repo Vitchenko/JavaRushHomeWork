fa58ddc03da199010e89b95711756985f616e8ef
Vitchenko <Euraeuspa12!>
2016-12-16 11:50:23 +0200

    task_10

---
+++ src/com/javarush/test/level31/lesson15/big01/Archiver.java
@@ -0,0 +1,27 @@
+package com.javarush.test.level31.lesson15.big01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Archiver
+{
+    public static void main(String[] args) throws Exception
+    {
+
+        System.out.println("Введите путь к файлу архива?");
+        BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+        String textPath=bf.readLine();
+
+        Path path=Paths.get(textPath);
+        ZipFileManager zipFileManager=new ZipFileManager(path);
+
+        System.out.println("Введите путь к файлу который будем архивировать?");
+        String textPath2=bf.readLine();
+
+        zipFileManager.createZip(Paths.get(textPath2));
+    }
+
+}
---
+++ src/com/javarush/test/level31/lesson15/big01/ZipFileManager.java
@@ -0,0 +1,20 @@
+package com.javarush.test.level31.lesson15.big01;
+
+import java.nio.file.Path;
+
+/**
+ * Created by viv on 16.12.2016.
+ */
+public class ZipFileManager
+{
+    private Path zipFile;
+
+    public ZipFileManager(Path zipFile)
+    {
+        this.zipFile = zipFile;
+    }
+
+    public void createZip(Path source) throws Exception{
+
+    }
+}
