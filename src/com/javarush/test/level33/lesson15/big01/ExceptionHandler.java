398437dc397635dc26caee0a447727062ccaa987
Vitchenko <Euraeuspa12!>
2016-12-17 13:30:40 +0200

    task_10

---
+++ src/com/javarush/test/level33/lesson15/big01/ExceptionHandler.java
@@ -0,0 +1,12 @@
+package com.javarush.test.level33.lesson15.big01;
+
+/**
+ * Created by viv on 17.12.2016.
+ */
+public class ExceptionHandler
+{
+    public static void log(Exception e){
+        System.out.println(e.toString());
+    }
+
+}
