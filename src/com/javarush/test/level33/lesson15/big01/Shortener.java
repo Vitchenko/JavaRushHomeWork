ff49dd1754c457d742c056ea940ab83f3f5cee47
Vitchenko <Euraeuspa12!>
2016-12-17 12:54:57 +0200

    task_10

---
+++ src/com/javarush/test/level33/lesson15/big01/Shortener.java
@@ -0,0 +1,16 @@
+package com.javarush.test.level33.lesson15.big01;
+
+/**
+ * Created by viv on 17.12.2016.
+ */
+public class Shortener
+{
+    public Long getId(String string){
+        return null;
+    }
+
+    public String getString(Long id){
+        return null;
+    }
+
+}
