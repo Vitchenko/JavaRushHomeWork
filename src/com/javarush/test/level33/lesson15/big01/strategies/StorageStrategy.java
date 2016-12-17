75e6080bccf35495992b512ce0ce11e1afb75a0a
Vitchenko <Euraeuspa12!>
2016-12-17 12:59:09 +0200

    task_10

---
+++ src/com/javarush/test/level33/lesson15/big01/strategies/StorageStrategy.java
@@ -0,0 +1,14 @@
+package com.javarush.test.level33.lesson15.big01.strategies;
+
+/**
+ * Created by viv on 17.12.2016.
+ */
+public interface StorageStrategy
+{
+    boolean containsKey(Long key);
+    boolean containsValue(String value);
+    void put(Long key, String value);
+    Long getKey(String value);
+    String getValue(Long key);
+
+}
