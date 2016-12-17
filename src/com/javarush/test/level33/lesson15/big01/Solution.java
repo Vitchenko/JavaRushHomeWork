63f610ca165620bbe3ea11c5f651116dc432f5d5
Vitchenko <Euraeuspa12!>
2016-12-17 16:12:51 +0200

    task_10

--- src/com/javarush/test/level33/lesson15/big01/Solution.java
+++ src/com/javarush/test/level33/lesson15/big01/Solution.java
@@ -1,45 +1,92 @@
 package com.javarush.test.level33.lesson15.big01;
 
-/**
- Первая стратегия готова, пришло время ее протестить. Для этого:
- 6.1.	Создай класс Solution.
- 6.2.	Добавь в класс Solution реализации вспомогательных статических методов:
- 6.2.1.	Set<Long> getIds(Shortener shortener, Set<String> strings). Этот метод должен
- для переданного множества строк возвращать множество
- идентификаторов. Идентификатор для каждой отдельной строки нужно
- получить, используя  shortener.
- 6.2.2.	Set<String> getStrings(Shortener shortener, Set<Long> keys). Метод будет
- возвращать множество строк, которое соответствует переданному
- множеству идентификаторов.
- При реальном использовании Shortener, задача получить из множества строк
- множество идентификаторов и наоборот скорее всего не встретится, это нужно
- исключительно для тестирования.
- 6.2.3.	testStrategy(StorageStrategy strategy, long elementsNumber). Метод будет
- тестировать работу переданной стратегии на определенном количестве
- элементов elementsNumber. Реализация метода должна:
- 6.2.3.1.	Выводить имя класса стратегии. Имя не должно включать имя пакета.
- 6.2.3.2.	Генерировать тестовое множество строк, используя Helper и заданное
- количество элементов elementsNumber.
- 6.2.3.3.	Создавать объект типа Shortener, используя переданную стратегию.
- 6.2.3.4.	Замерять и выводить время необходимое для отработки метода getIds
- для заданной стратегии и заданного множества элементов. Время
- вывести в миллисекундах. При замере времени работы метода можно
- пренебречь переключением процессора на другие потоки, временем,
- которое тратится на сам вызов, возврат значений и вызов методов
- получения времени (даты). Замер времени произведи с
- использованием объектов типа Date.
- 6.2.3.5.	Замерять и выводить время необходимое для отработки метода
- getStrings для заданной стратегии и полученного в предыдущем пункте
- множества идентификаторов.
- 6.2.3.6.	Сравнивать одинаковое ли содержимое множества строк, которое было
- сгенерировано и множества, которое было возвращено методом
- getStrings. Если множества одинаковы, то выведи "Тест пройден.",
- иначе "Тест не пройден.".
- 6.2.4.	Добавь метод main(). Внутри метода протестируй стратегию
- HashMapStorageStrategy с помощью 10000 элементов.
- 6.3.	Проверь, что программа работает и тест пройден.
 
- */
+import com.javarush.test.level33.lesson15.big01.strategies.HashMapStorageStrategy;
+import com.javarush.test.level33.lesson15.big01.strategies.StorageStrategy;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+
 public class Solution
 {
+    public static void main(String[] args)
+    {
+        StorageStrategy strategy = new HashMapStorageStrategy();
+        testStrategy(strategy, 10000);
+    }
+
+    public static Set<Long> getIds(Shortener shortener, Set<String> strings){
+
+        Set<Long> setLong=new HashSet<>();
+
+        for(String s:strings){
+            setLong.add(shortener.getId(s));
+        }
+        return setLong;
+    }
+
+    public static Set<String> getStrings(Shortener shortener, Set<Long> keys){
+        Set<String> setStr=new HashSet<>();
+
+        for(Long l:keys){
+            setStr.add(shortener.getString(l));
+        }
+        return setStr;
+    }
+
+    public static void testStrategy(StorageStrategy strategy, long elementsNumber){
+
+        //Выводить имя класса стратегии. Имя не должно включать имя пакета
+        Helper.printMessage(strategy.getClass().getSimpleName());
+
+        //Генерировать тестовое множество строк, используя Helper и заданное количество элементов elementsNumber
+        Set<String> testSetStrings = new HashSet<>();
+        for (int i = 0; i < elementsNumber; i++) {
+            testSetStrings.add(Helper.generateRandomString());
+        }
+
+        //Создавать объект типа Shortener, используя переданную стратегию
+        Shortener shortener = new Shortener(strategy);
+
+
+        //Замерять и выводить время необходимое для отработки метода getIds для заданной стратегии и заданного множества элементов
+        //Время вывести в миллисекундах. При замере времени работы метода можно пренебречь переключением процессора на другие потоки,
+        // временем, которое тратится на сам вызов, возврат значений и вызов методов получения времени (даты).
+        // Замер времени произведи с использованием объектов типа Date.
+
+        Set<Long> idsSet;
+
+        Date startTime1 = new Date();
+        idsSet = getIds(shortener, testSetStrings);
+        Date finishTime1 = new Date();
+
+        long msDelay1 = finishTime1.getTime() - startTime1.getTime();
+        Helper.printMessage(Long.toString(msDelay1));
+
+
+        //Замерять и выводить время необходимое для отработки метода getStrings для заданной стратегии и
+        //полученного в предыдущем пункте множества идентификаторов.
+        Set<String> stringSet;
+        Date startTime2 = new Date();
+        stringSet = getStrings(shortener, idsSet);
+        Date finishTime2 = new Date();
+
+        long msDelay2 = finishTime2.getTime() - startTime2.getTime();
+        Helper.printMessage(Long.toString(msDelay2));
+
+
+        //Сравнивать одинаковое ли содержимое множества строк, которое было сгенерировано
+        // и множества, которое было возвращено методом getStrings.
+        // Если множества одинаковы, то выведи "Тест пройден.", иначе "Тест не пройден.".
+        if (testSetStrings.equals(stringSet)) {
+            Helper.printMessage("Тест пройден.");
+        } else {
+            Helper.printMessage("Тест не пройден.");
+        }
+
+
+    }
+
 }
