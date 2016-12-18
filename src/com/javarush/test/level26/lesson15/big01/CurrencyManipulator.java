76d302f8e4a0519bff55d18d729107c8c1d0bd3e
Vitchenko <Euraeuspa12!>
2016-12-18 18:42:30 +0200

    task_10

--- src/com/javarush/test/level26/lesson15/big01/CurrencyManipulator.java
+++ src/com/javarush/test/level26/lesson15/big01/CurrencyManipulator.java
@@ -3,9 +3,7 @@ package com.javarush.test.level26.lesson15.big01;
 import com.javarush.test.level26.lesson15.big01.exception.NotEnoughMoneyException;
 
 import java.math.BigInteger;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentNavigableMap;
 import java.util.concurrent.ConcurrentSkipListMap;
 
@@ -14,12 +12,69 @@ public class CurrencyManipulator
 {
 
     private String currencyCode;
-    private Map<Integer, Integer> denominations=new HashMap<Integer, Integer>();
+    private Map<Integer, Integer> denominations = new HashMap<>();
+
+    public boolean isAmountAvailable(int expectedAmount)
+    {
+        return expectedAmount <= getTotalAmount();
+    }
+
+    public Map<Integer, Integer> withdrawAmount(int expectedAmount) throws NotEnoughMoneyException
+    {
+        int sum = expectedAmount;
+        HashMap<Integer, Integer> temp = new HashMap<>();
+        temp.putAll(denominations);
+        ArrayList<Integer> list = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> pair : temp.entrySet())
+            list.add(pair.getKey());
+
+        Collections.sort(list);
+        Collections.reverse(list);
+
+        TreeMap<Integer, Integer> result = new TreeMap<>(new Comparator<Integer>()
+        {
+            @Override
+            public int compare(Integer o1, Integer o2)
+            {
+                return o2.compareTo(o1);
+            }
+        });
+
+        for (Integer aList : list) {
+            int key = aList;
+            int value = temp.get(key);
+            while (true) {
+                if (sum < key || value <= 0) {
+                    temp.put(key, value);
+                    break;
+                }
+                sum -= key;
+                value--;
+
+                if (result.containsKey(key))
+                    result.put(key, result.get(key) + 1);
+                else
+                    result.put(key, 1);
+            }
+        }
+
+        if (sum > 0)
+            throw new NotEnoughMoneyException();
+        else
+        {
+            for (Map.Entry<Integer, Integer> pair : result.entrySet())
+                ConsoleHelper.writeMessage("\t" + pair.getKey() + " - " + pair.getValue());
+
+            denominations.clear();
+            denominations.putAll(temp);
+            ConsoleHelper.writeMessage("Transaction was successful!");
+        }
+        return result;
+    }
 
     public CurrencyManipulator(String currencyCode)
     {
         this.currencyCode = currencyCode;
-        //this.denominations=new HashMap<>();
     }
 
     public String getCurrencyCode()
@@ -45,98 +100,4 @@ public class CurrencyManipulator
     public boolean hasMoney(){
         return denominations.size() != 0;
     }
-
-    public boolean isAmountAvailable(int expectedAmount) {
-        return getTotalAmount() >= expectedAmount;
-        //, который вернет true, если денег достаточно для выдачи.
-    }
-
-
-     public Map<Integer, Integer> withdrawAmount ( int expectedAmount) throws NotEnoughMoneyException {
-
-        ConcurrentNavigableMap<Integer, Integer> map = ((ConcurrentSkipListMap<Integer, Integer>) denominations).descendingMap();
-        if (map.isEmpty()) throw new NotEnoughMoneyException();
-        Integer[] values, ammounts;
-        int price, position, count = map.keySet().size();
-        values = map.keySet().toArray(new Integer[count]);
-        ammounts = map.values().toArray(new Integer[count]);
-        //Greedy algorithm
-        List<Integer[]> variations = WithdrawSolutions.withdrawSolutions(values, ammounts, new int[count], expectedAmount, 0);
-        if (variations.isEmpty())
-            throw new NotEnoughMoneyException();
-
-        //Finding best choice
-
-        // Find the smallest number of banknotes in variations
-        int minBanknotesNumber = Integer.MAX_VALUE;
-        int[] bankNotesNumbers = new int[variations.size()];
-        // and compute hash to identify variation with more large denomination baknotes
-        int step = 0;
-        for (Integer v : map.values())
-        {
-            if (v > step) step = v;
-        }
-        BigInteger[] hashes = new BigInteger[variations.size()];
-        for (int i = 0; i < hashes.length; i++)
-        {
-            hashes[i] = BigInteger.ZERO;
-        }
-        for (int i = 0; i < variations.size(); i++)
-        {
-            Integer[] variationsI = variations.get(i);
-            BigInteger base = BigInteger.valueOf(1);
-            int banknotesNumberI = 0;
-            for (Integer var : variationsI)
-            {
-                if (var != 0)
-                {
-                    hashes[i] = hashes[i].add(base.multiply(BigInteger.valueOf(var)));
-                    banknotesNumberI += var;
-                }
-                base = base.multiply(BigInteger.valueOf(step));
-            }
-            bankNotesNumbers[i] = banknotesNumberI;
-            if (banknotesNumberI < minBanknotesNumber) minBanknotesNumber = banknotesNumberI;
-        }
-        // Best variation must have biggest hash among variations with smallest number of banknotes
-        int maxHashI = -1;
-        for (int i = 0; i < hashes.length; i++)
-            if (bankNotesNumbers[i] == minBanknotesNumber)
-            {
-                if (maxHashI == -1)
-                { //first match
-                    maxHashI = i;
-                    continue;
-                }
-                BigInteger hash = hashes[i];
-                if (hash.compareTo(hashes[maxHashI]) > 0)
-                    maxHashI = i;
-            }
-        Integer[] bestChoice = variations.get(maxHashI);
-
-        // Fill in best choice into result variable
-        Map<Integer, Integer> withdrawBanknotes = new HashMap<Integer, Integer>();
-        for (int i = bestChoice.length - 1; i >= 0; i--)
-        {
-            Integer b = bestChoice[i];
-            if (b != 0)
-                withdrawBanknotes.put(values[i], b);
-        }
-
-        // withdraw result from CurrencyManipulator
-        for (Integer key : withdrawBanknotes.keySet())
-        {
-            int newAmmount = denominations.get(key) - withdrawBanknotes.get(key);
-            if (newAmmount == 0) denominations.remove(key);
-            else denominations.put(key, newAmmount);
-        }
-
-        return withdrawBanknotes;
-    }
-
-
-        public void clear() {
-        denominations = new ConcurrentSkipListMap<Integer, Integer>();
-    }
-
 }
--- src/com/javarush/test/level26/lesson15/big01/command/WithdrawCommand.java
+++ src/com/javarush/test/level26/lesson15/big01/command/WithdrawCommand.java
@@ -10,54 +10,47 @@ import java.util.*;
 
 class WithdrawCommand implements Command
 {
-
- /**before=Withdrawing...
-  success.format=%d %s was withdrawn successfully
-  specify.amount=Please specify integer amount for withdrawing.
-  specify.not.empty.amount=Please specify valid positive integer amount for withdrawing.
-  not.enough.money=Not enough money on your account, please try again
-  exact.amount.not.available=Exact amount is not available*/
-
+    //private ResourceBundle res = ResourceBundle.getBundle(CashMachine.RESOURCE_PATH + "withdraw_en");
     @Override
-    public void execute() throws InterruptOperationException{
+    public void execute() throws InterruptOperationException
+    {
+        ConsoleHelper.writeMessage("Enter currency code");
         String currencyCode = ConsoleHelper.askCurrencyCode();
-        CurrencyManipulator manipulator = CurrencyManipulatorFactory.getManipulatorByCurrencyCode(currencyCode);
-        ConsoleHelper.writeMessage("Withdrawing...");
-        int sum = 0;
-        do {
-            ConsoleHelper.writeMessage("Please specify integer amount for withdrawing.");
-            try {
-                sum = Integer.parseInt(ConsoleHelper.readString());
-                if (sum < 0) throw new NumberFormatException("Please specify valid positive integer amount for withdrawing.");
-                if (!manipulator.isAmountAvailable(sum)) {
-                    ConsoleHelper.writeMessage("Not enough money on your account, please try again");
-                    continue;
-                }
-                Map<Integer, Integer> withdrawBanknotes = manipulator.withdrawAmount(sum);
-                withdrawBanknotes.keySet();
-                /*Вывести пользователю результат из п. 1.3.4. в следующем виде:
-                <табуляция>номинал - количество
-                Сортировка - от большего номинала к меньшему.
-                Sort by key (denominations) */
-                List list = new ArrayList(withdrawBanknotes.entrySet());
-                Collections.sort(list, new Comparator<Map.Entry<Integer, Integer>>() {
-                    @Override
-                    public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
-                        return o2.getKey() - o1.getKey();
-                    }
-                });
-
-                for (Object o : list) {
-                    Map.Entry<Integer, Integer> entry = (Map.Entry<Integer, Integer>)o;
-                    ConsoleHelper.writeMessage(String.format("\t%d - %d", entry.getKey(), entry.getValue()));
-                }
-                ConsoleHelper.writeMessage(String.format("%d %s was withdrawn successfully", sum, manipulator.getCurrencyCode()));
-                break; //If reached - It's ok. Goto end;
-            } catch (NumberFormatException e) {
-                ConsoleHelper.writeMessage("Please specify valid positive integer amount for withdrawing.");
-            } catch (NotEnoughMoneyException e) {
-                ConsoleHelper.writeMessage("Exact amount is not available");
+        CurrencyManipulator currencyManipulator = CurrencyManipulatorFactory.getManipulatorByCurrencyCode(currencyCode);
+        int sum;
+        while(true)
+        {
+            ConsoleHelper.writeMessage("Withdrawing...");
+            String s = ConsoleHelper.readString();
+            try
+            {
+                sum = Integer.parseInt(s);
+            } catch (NumberFormatException e)
+            {
+                ConsoleHelper.writeMessage("Please specify integer amount for withdrawing.");
+                continue;
             }
-        } while (true);
+            if (sum <= 0)
+            {
+                ConsoleHelper.writeMessage("Please specify valid positive integer amount for withdrawing.");
+                continue;
+            }
+            if (!currencyManipulator.isAmountAvailable(sum))
+            {
+                ConsoleHelper.writeMessage("Not enough money on your account, please try again");
+                continue;
+            }
+            try
+            {
+                currencyManipulator.withdrawAmount(sum);
+            } catch (NotEnoughMoneyException e)
+            {
+                ConsoleHelper.writeMessage("Exact amount is not available");
+                continue;
+            }
+            ConsoleHelper.writeMessage(String.format("%d %s was withdrawn successfully", sum, currencyCode));
+            break;
+        }
+
     }
 }
