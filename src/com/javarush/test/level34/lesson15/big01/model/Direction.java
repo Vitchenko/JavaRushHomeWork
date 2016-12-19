0afd864e8e4b222bd6c6b0021173ebcb2cf0032b
Vitchenko <Euraeuspa12!>
2016-12-19 23:08:32 +0200

    task_10

---
+++ src/com/javarush/test/level34/lesson15/big01/model/Direction.java
@@ -0,0 +1,9 @@
+package com.javarush.test.level34.lesson15.big01.model;
+
+/**
+ * Created by viv on 19.12.2016.
+ */
+public enum Direction
+{
+    LEFT, RIGHT, UP, DOWN
+}
---
+++ src/com/javarush/test/level34/lesson15/big01/model/Movable.java
@@ -0,0 +1,10 @@
+package com.javarush.test.level34.lesson15.big01.model;
+
+/**
+ * Created by viv on 19.12.2016.
+ */
+public interface Movable
+{
+    void move(int x, int y);
+
+}
