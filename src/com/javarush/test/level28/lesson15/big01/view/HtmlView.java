04fb6ec95547f958ae75e05f51d22eaa88ba66c0
Vitchenko <Euraeuspa12!>
2016-12-19 22:17:54 +0200

    task_10

---
+++ src/com/javarush/test/level28/lesson15/big01/view/HtmlView.java
@@ -0,0 +1,26 @@
+package com.javarush.test.level28.lesson15.big01.view;
+
+import com.javarush.test.level28.lesson15.big01.Controller;
+import com.javarush.test.level28.lesson15.big01.vo.Vacancy;
+
+import java.util.List;
+
+/**
+ * Created by viv on 19.12.2016.
+ */
+public class HtmlView implements View
+{
+    private Controller controller;
+
+    @Override
+    public void update(List<Vacancy> vacancies)
+    {
+
+    }
+
+    @Override
+    public void setController(Controller controller)
+    {
+        this.controller=controller;
+    }
+}
