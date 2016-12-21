673061c199e713ac40d59a78f16c535768c0ad38
Vitchenko <Euraeuspa12!>
2016-12-21 15:14:57 +0200

    task_10

--- src/com/javarush/test/level35/lesson08/bonus01/ConvertableUtil.java
+++ src/com/javarush/test/level35/lesson08/bonus01/ConvertableUtil.java
@@ -6,8 +6,28 @@ import java.util.Map;
 
 public class ConvertableUtil {
 
-    public static Map convert(List list) {
-        Map result = new HashMap();
-        return result;
+
+    public static Map convert(List <? extends Convertable> list ) {
+
+        Map<Object, Convertable<?>> result = new HashMap();
+
+        for (Convertable<?> key : list) {
+            result.put(key.getKey(), key);
+        }
+       return result;
     }
 }
+/* List to Map
+        Реализуйте логику метода convert в классе ConvertableUtil, который должен возвращать словарь,
+        значениями которого являются элементы переданного Списка,
+        а ключами являются объекты, полученные вызовом интерфейсного метода getKey.
+
+        Элементы Списка должны наследоваться от Convertable, который в свою очередь параметризирован каким-то ключом.
+        Например, ConvertableBook параметризирован String, т.е. ключ в результирующем словаре должен иметь тип - String
+        ConvertableUser параметризирован Integer, т.е. ключ в результирующем словаре должен иметь тип - Integer
+
+        Значения в словаре должны совпадать с элементами Списка.
+        Смотрите метод main для примера.
+
+        Расставьте в методе ConvertableUtil.convert дженерик типы.
+        */
