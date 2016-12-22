020ac247269ef6270a3ae95789415e86ceb5a2eb
Vitchenko <Euraeuspa12!>
2016-12-22 11:35:08 +0200

    task_10

---
+++ src/com/javarush/test/level36/lesson04/big01/controller/Controller.java
@@ -0,0 +1,20 @@
+package com.javarush.test.level36.lesson04.big01.controller;
+
+import com.javarush.test.level36.lesson04.big01.model.Model;
+
+/**
+ * Created by viv on 22.12.2016.
+ */
+public class Controller
+{
+    private Model model;
+
+    public void setModel(Model model)
+    {
+        this.model = model;
+    }
+
+    public void onShowAllUsers(){
+        model.loadUsers();
+    }
+}
