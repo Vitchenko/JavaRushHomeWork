f5b0806267e72429c71c2dcf7f7fa92730286872
Vitchenko <Euraeuspa12!>
2016-12-24 09:41:43 +0200

    task_10

--- src/com/javarush/test/level31/lesson15/big01/command/ZipContentCommand.java
+++ src/com/javarush/test/level31/lesson15/big01/command/ZipContentCommand.java
@@ -3,7 +3,7 @@ package com.javarush.test.level31.lesson15.big01.command;
 /**
  * Created by viv on 23.12.2016.
  */
-public class ZipContentCommand implements Command
+public class ZipContentCommand extends ZipCommand
 {
     @Override
     public void execute() throws Exception
